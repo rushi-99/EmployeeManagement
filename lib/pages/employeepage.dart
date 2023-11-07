836d90de96e3808d96f33ba4d77d19ccb6858858
Rushini Wickramasooriya <51336432+rushi-99@users.noreply.github.com>
2023-11-07 13:08:04 +0530

    search function not working - fixed

--- lib/pages/employeepage.dart
+++ lib/pages/employeepage.dart
@@ -3,6 +3,7 @@ import 'package:employeemanagement/model/employees.dart';
 import 'package:employeemanagement/services/service.dart';
 import 'package:employeemanagement/pages/editEmployee.dart';
 import 'package:employeemanagement/pages/createEmployeePage.dart';
+import 'package:employeemanagement/model/employees.dart';
 
 
 class EmployeePage extends StatefulWidget {
@@ -12,35 +13,68 @@ class EmployeePage extends StatefulWidget {
 
 class _EmployeePageState extends State<EmployeePage> {
   final TextEditingController _searchController = TextEditingController();
-  bool _refreshing = false;
   final FetchEmployees _employees = FetchEmployees();
-
-  // List<Employees> _filteredEmployed
+  List<Employees> emp = <Employees> [];
+  String searchKeyword = '';
 
   @override
   void initState(){
     super.initState();
-    _refreshEmployees();
+    fetchEmployeeData();
   }
 
   Future<void> _refreshEmployees() async {
-    await _employees.getEmployees();
-    setState(() {
-      _refreshing = true;
-    });
-
-    await _employees.getEmployees();
-
-    setState(() {
-      _refreshing = false;
-    });// You may need to update this to fetch the employees again.
+    try {
+      var result = await _employees.getEmployees();
+      setState(() {
+        emp = result;
+      });
+    } catch (e) {
+      print(e);
+    }
   }
 
-  // void updateList(String value){
+  // void filteration(String enterKeyword){
+  //   List<Employees> results = [];
+  //   if(enterKeyword.isEmpty){
+  //     results = emp;
+  //   }else{
+  //     results = emp
+  //         .where((employee) => employee.empName!.toLowerCase().contains(enterKeyword.toLowerCase()))
+  //         .toList();
+  //   }
   //   setState(() {
-  //     display_list =
+  //     emp = results;
   //   });
   // }
+
+  Future<void> fetchEmployeeData() async{
+    try{
+      print('hello');
+      var result = await _employees.getEmployees();
+      print(result);
+      setState(() {
+        emp = result;
+        print(emp);
+      });
+
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void filterEmployees(String searchKeyword) {
+    // Filter the employees based on the search keyword and update the list.
+    List<Employees> filteredEmployees = emp
+        .where((employee) =>
+        employee.empName!.toLowerCase().contains(searchKeyword.toLowerCase()))
+        .toList();
+
+    setState(() {
+      emp = filteredEmployees;
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -66,97 +100,102 @@ class _EmployeePageState extends State<EmployeePage> {
                   _refreshEmployees();
                 }
               });
-              },
+            },
           )
         ],
       ),
-      body:
-      RefreshIndicator(
+      body: RefreshIndicator(
         onRefresh: _refreshEmployees,
-          child: FutureBuilder<List<Employees>>(
-            future: _employees.getEmployees(),
-            builder: (context, snapshot){
-              var data = snapshot.data;
-              return Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: TextField(
-                      style: TextStyle(height: 1.0,),
-                      controller: _searchController,
-                      decoration: InputDecoration(
-                        fillColor: Colors.pink[50],
-                        filled: true,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                          borderSide: BorderSide.none,
-                        ),
-                        hintText: 'search employees',
-                        prefixIcon: Icon(Icons.search),
+        // child: FutureBuilder<List<Employees>>(
+        //   future: _employees.getEmployees(),
+        //   builder: (context, snapshot){
+        //     var data = snapshot.data;
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextField(
+                    controller: _searchController,
+                    onChanged: (value) {
+                      filterEmployees(value);
+                    },
+                    style: TextStyle(height: 1.0,),
+                    decoration: InputDecoration(
+                      fillColor: Colors.pink[50],
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide.none,
                       ),
+                      hintText: 'search employees',
+                      prefixIcon: Icon(Icons.search),
                     ),
                   ),
-                  Expanded(
-                    child: ListView.builder(
-                        itemCount: data?.length,
-                        itemBuilder: (context,index)
-                        {
-                          if(!snapshot.hasData){
-                            return Center(child: CircularProgressIndicator());
-                          }
+                ),
+                Expanded(
+                  child: ListView.builder(
+                      itemCount: emp?.length,
+                      itemBuilder: (context,index) {
+                        if (searchKeyword.isEmpty || emp[index].empName!.toLowerCase().contains(searchKeyword.toLowerCase())) {
                           return Padding(
                             padding: const EdgeInsets.all(2.0),
-                            child:Card(
+                            child: Card(
                               child: ListTile(
                                 title: Row(
                                     children: [
-                                        Column(
-                                          crossAxisAlignment: CrossAxisAlignment.start,
-                                          children: [
-                                            Text('${data?[index].empName}',
-                                            style: TextStyle(fontWeight: FontWeight.bold),),
-                                            Text('${data?[index].empNo}'),
-                                          ],
-                                        )
-                                      ]
-                                    ),
-                                    trailing: Row(
-                                      mainAxisSize: MainAxisSize.min,
+                                      Column(
+                                        crossAxisAlignment: CrossAxisAlignment
+                                            .start,
                                         children: [
-                                          IconButton(
-                                            onPressed: () {
-                                              print((data?[index].empNo).toString());
-                                              String res = (data?[index].empNo).toString();
-                                              deleteEmp(res);
-                                            },
-                                            icon: const Icon(Icons.delete),
-                                            color: Colors.red[400],
-                                            tooltip: 'Delete Employee',
-                                          ),
-                                          IconButton(
-                                            onPressed: () {
-                                              Navigator.push(
-                                                  context,
-                                                  MaterialPageRoute(
-                                                    builder: (context)=> EditEmployeePage(text : data?[index].empNo),
-                                              ));
-                                            },
-                                            icon: const Icon(Icons.edit),
-                                            color: Colors.blue[400],
-                                            tooltip: 'Edit Employee',
-                                          ),
+                                          Text('${emp?[index].empName}',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold),),
+                                          Text('${emp?[index].empNo}'),
                                         ],
-                                    ),
-                                  ),
+                                      )
+                                    ]
                                 ),
-                              );
-                            }),
-                  ),
-                ],
-              );
-                },
-              ),
-          ),
+                                trailing: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    IconButton(
+                                      onPressed: () {
+                                        print((emp?[index].empNo).toString());
+                                        String res = (emp?[index].empNo)
+                                            .toString();
+                                        deleteEmp(res);
+                                      },
+                                      icon: const Icon(Icons.delete),
+                                      color: Colors.red[400],
+                                      tooltip: 'Delete Employee',
+                                    ),
+                                    IconButton(
+                                      onPressed: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) =>
+                                                  EditEmployeePage(
+                                                      text: emp?[index].empNo),
+                                            ));
+                                      },
+                                      icon: const Icon(Icons.edit),
+                                      color: Colors.blue[400],
+                                      tooltip: 'Edit Employee',
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                      }else{
+                          return SizedBox.shrink();
+                        }
+                      }),
+                ),
+              ],
+            ),
+      ),
     );
   }
 
@@ -182,8 +221,4 @@ class _EmployeePageState extends State<EmployeePage> {
     final snackBar = SnackBar(content: Text(message));
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
-
-  // Widget getCard(){
-  //
-  // }
 }
